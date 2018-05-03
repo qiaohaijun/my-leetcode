d39a18219140f07a68d7f70343c1870b941484c9
coding <coding@coding.net>
2018-05-03 16:06:35 +0000

    format code

--- solution_pool/1_two_sum/Solution.cc
+++ solution_pool/1_two_sum/Solution.cc
@@ -1,6 +1,6 @@
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        std::cerr << "hello world" << std::endl;
-}
+  public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    std::cerr << "hello world" << std::endl;
+  }
 };
--- solution_pool/203-remove-linked-list-elements/solution.cc
+++ solution_pool/203-remove-linked-list-elements/solution.cc
@@ -1,1 +1,28 @@
-
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
+class Solution {
+  public:
+  ListNode* removeElements(ListNode* head, int val) {
+    ListNode* dummy_node = new ListNode(-1);
+    dummy_node->next = head;
+    ListNode* prev_node = dummy_node;
+    ListNode* cur_node = dummy_node->next;
+    while(cur_node!=NULL)
+    {
+      if(cur_node->val == val){
+        prev_node->next = cur_node->next;
+        cur_node = prev_node->next;
+      } else {
+        prev_node = cur_node;
+        cur_node = cur_node->next;
+      }
+    }
+    return dummy_node->next;
+  }
+};
