076bb6a4bf308cf8b59762b21f4cd3686a974457
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2018-05-06 16:49:38 +0800

    Update solution.cc

--- solution_pool/283-move-zeroes/solution.cc
+++ solution_pool/283-move-zeroes/solution.cc
@@ -1,6 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+      // guard
+      if(nums.size()==0){
+        return;
+      }
       int i = 0;
       for(int j =0; j< nums.size(); ++j){
         if(nums[j] == 0) {
@@ -11,7 +15,7 @@ public:
         }
                 
       }
-      
+      // add '0'
       for(int k = i; k < nums.size();++k) {
         nums[k] =0;
       }
