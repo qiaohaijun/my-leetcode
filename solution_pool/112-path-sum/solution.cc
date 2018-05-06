08363251dc14b394195b81b91bd5a7876d81ed06
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2018-05-06 20:59:49 +0800

    Create solution.cc

---
+++ solution_pool/112-path-sum/solution.cc
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isLeaf(TreeNode* root) {
+      if(root->left == NULL && root->right == NULL) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  
+    bool hasPathSum(TreeNode* root, int sum) {
+      if(root == NULL){
+        return false;
+      }
+      
+      if(isLeaf(root) ){
+        if(root->val ==sum){
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return hasPathSum(root->left, sum- root->val) || hasPathSum(root->right, sum-root->val);
+      }
+      
+      
+    }
+};
