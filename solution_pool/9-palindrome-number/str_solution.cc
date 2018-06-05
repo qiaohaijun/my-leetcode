aa3b7286088502371f5287bd7c3e9650f3b2bb35
qiaohaijun <qiaohaijun@sogou-inc.com>
2018-06-05 15:06:05 +0800

    str_solution.cc format by clang-format

--- solution_pool/9-palindrome-number/str_solution.cc
+++ solution_pool/9-palindrome-number/str_solution.cc
@@ -1,22 +1,21 @@
 class Solution {
-public:
-    bool isPalindrome(int x) {
+ public:
+  bool isPalindrome(int x) {
 
-      
-      // guard
-      if(x<0){
+    // guard
+    if (x < 0) {
+      return false;
+    }
+    string x_str = std::to_string(x);
+    int i = 0;
+    int j = x_str.size() - 1;
+    while (i < j) {
+      if (x_str[i] != x_str[j]) {
         return false;
       }
-      string x_str = std::to_string(x);
-      int i = 0;
-      int j = x_str.size()-1;
-      while(i<j){
-        if(x_str[i]!=x_str[j]){
-          return false;
-        }
-        i++;
-        j--;
-      }
-      return true;
+      i++;
+      j--;
     }
+    return true;
+  }
 };
