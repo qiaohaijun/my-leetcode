3070e54c5507271dfc583ff10a8452391a7c5198
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2018-06-07 23:25:59 +0800

    Create solution.cc

---
+++ solution_pool/136-single-number/solution.cc
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+      int ret = 0;
+      for(int i=0; i<nums.size(); ++i){
+        ret=ret^nums.at(i);
+      }
+      return ret;
+    }
+};
