5bdcb176a35ca11fe99c4a9bd3aacdaf74659222
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2018-11-14 23:28:23 +0800

    Create slide_window.py

---
+++ solution_pool/3_longest-substring-without-repeating-characters/slide_window.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        table = dict()
+        
+        slow = 0
+        fast = 0
+        ret_max = 0
+        
+        while slow<len(s) and fast<len(s):
+          # find dup, slide window
+          if s[fast] in table:
+            del(table[s[slow]])
+            slow+=1
+          else:
+            table[s[fast]] = fast
+            fast+=1
+            ret_max = max(ret_max,fast-slow)
+            
+        return ret_max
