c8b0308ef791d130e7f3390ce20b6e55fa878e46
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2019-02-18 22:42:28 +0800

    Create 0007-reverse-integer.py

---
+++ python_solution/0007-reverse-integer.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+        is_negative = False
+        if x<0:
+          is_negative = True
+          x = x*-1
+        num = 0
+        
+        
+        while x!=0:
+          num = x%10 + num*10
+          x=x/10
+          
+        if num > pow(2,31)-1 or num < pow(-2,31):
+          return 0
+        if is_negative == True:
+          return -1*num
+        else:
+          return num
