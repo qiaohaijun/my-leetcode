5c49a3932959a67cbf54eda5dae073e123a21c66
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2019-03-09 14:01:11 +0800

    Update 0198-house-robber.py
     增加代码注释.

--- python_solution/0198-house-robber.py
+++ python_solution/0198-house-robber.py
@@ -17,7 +17,9 @@ class Solution(object):
         dp = [0]*len(nums)
         
         # boundary condition
+        # 只有一个房子的时候, 作为有责任感的小偷, 我们坚决不空手而归. 有一个偷一个.
         dp[0] = nums[0]
+        # 当只有两个房子的时候, 我们要去偷最有钱的房子
         dp[1] = max(nums[0], nums[1])
         
         # dp 
