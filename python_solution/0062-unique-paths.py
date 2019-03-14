58bdbbe5d0411dd1a46adf36c5e45848ab2d686c
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2019-03-14 11:40:13 +0800

    代码贴错了

--- python_solution/0062-unique-paths.py
+++ python_solution/0062-unique-paths.py
@@ -1,33 +1,31 @@
 class Solution(object):
-    def minPathSum(self, grid):
+    def uniquePaths(self, m, n):
         """
-        :type grid: List[List[int]]
+        :type m: int
+        :type n: int
         :rtype: int
         """
-        # 注意初始化
-        # m x n
         
-        m = len(grid)
-        n = len(grid[0])
+        # grid大小 mxn
         
-        # init dp
-        dp = [[0]*n for _ in range(m)]        
-        dp[0][0] = grid[0][0]
+        dp = [[0]*n for _ in range(m)]
         
-
-        # 初始化边界的情况,避免padding
-        # init row
-        for i in range(1,n):
-          dp[0][i] = dp[0][i-1]+grid[0][i]
+        # 这个题目的状态转移方程不难
+        # 需要注意的就是边界的初始化过程
+        # 边界条件, 这个表示开始的位置只有一个办法到达
+        dp[0][0] = 1
+        
+        # 因为只能向下和向右走, 所以初始化一下边界条件
+        for i in range(1,m):
+          dp[i][0] = 1
+        
+        for j in range(1,n):
+          dp[0][j] = 1
           
-        # init col
-        for j in range(1,m):
-          dp[j][0] = dp[j-1][0] + grid[j][0]
-          
-        # 打表剩下的位置
         for i in range(1,m):
           for j in range(1,n):
-          
-            dp[i][j] = min(dp[i-1][j],dp[i][j-1])+grid[i][j]
-              
+            dp[i][j] += dp[i-1][j] + dp[i][j-1]
+            
         return dp[-1][-1]
+          
+          
