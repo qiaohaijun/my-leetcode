ddcc0d935f771fb4cde17dac6cb4b7626dc3e839
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2019-03-19 21:13:16 +0800

    Create 0020-valid-parentheses-good.py

---
+++ python_solution/0020-valid-parentheses-good.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s):
+
+        d = {'}': '{', ']': '[', ')': '('}
+
+        stk = [' ']
+
+        for c in s:
+
+            c2 = d.get(c, '')
+            if stk[-1] == c2:
+                stk.pop()
+            elif c2:
+                return False
+            else:
+                stk.append(c)
+
+        return len(stk) == 1
