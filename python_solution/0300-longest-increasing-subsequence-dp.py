b18ae974189aaf5d8756dfccc12eb7afec519cde
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2019-03-19 20:02:31 +0800

    bug fix
    原来的代码是有问题的, 只是碰巧蒙对了而已

--- python_solution/0300-longest-increasing-subsequence-dp.py
+++ python_solution/0300-longest-increasing-subsequence-dp.py
@@ -9,8 +9,7 @@ class Solution(object):
           return 0
         
         # 所有的单个的nums[i] 都可以构成一个lis, 长度为1
-        # 但是如果改为dp =[1]*n,结果是错误的,需要找找原因.
-        dp = [0]*n
+        dp = [1]*n
         # dp初始化条件, 如果只有一个元素, 那么这个dp[0]=1
         dp[0] = 1
         max_lis = 1
@@ -19,9 +18,8 @@ class Solution(object):
           for j in range(0,i):
             # 对于每一个小于nums[i] >nums[j], 更新公式
             if(nums[i]>nums[j]):
-              dp[i] = max(dp[i],dp[j])
-              
-          dp[i]+=1
+              if(dp[j]+1>dp[i]):
+                dp[i] = dp[j]+1
           max_lis = max(max_lis,dp[i])
-      
+          
         return max_lis
