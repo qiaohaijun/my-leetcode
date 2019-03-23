8f58d366dc59db0a68e04b4b7f3cdb176d72719b
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2019-03-23 19:56:25 +0800

    简单清晰容易懂的代码
    这里的代码框架采用了最长回文子串的框架

--- python_solution/0516-longest-palindromic-subsequence.py
+++ python_solution/0516-longest-palindromic-subsequence.py
@@ -4,21 +4,27 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        
         n = len(s)
-        max_len  =0 
-        dp = [[0]*(n) for _ in range(n)]
         
+        dp = [[0]*n for _ in range(n)]
         
         # init 1
         for i in range(n):
             dp[i][i] = 1
+            
+        for i in range(1,n):
+            # 如果两者相同
+            if s[i-1]==s[i]:
+                dp[i-1][i] = 2
+            # 注意不要遗漏这个位置的代码, 不然会有代码会有 bug
+            else:
+                dp[i-1][i] = 1
         
-        # 需要根据递归公式的形式来填记录表
-        for i in range(n-1,-1,-1):
-            for j in range(i+1,n):
+        for l in range(3,n+1):
+            for i in range(0,n-l+1):
+                j = i+l-1
                 if s[i] == s[j]:
                     dp[i][j] = dp[i+1][j-1]+2
                 else:
                     dp[i][j] = max(dp[i+1][j],dp[i][j-1])
-        return dp[0][-1]
+        return dp[0][n-1]
