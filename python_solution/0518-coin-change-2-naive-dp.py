7f21c03556254f6ba7f8393cfa2aa4078e307ae8
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2019-03-24 11:54:53 +0800

    更新naive实现代码

--- python_solution/0518-coin-change-2-naive-dp.py
+++ python_solution/0518-coin-change-2-naive-dp.py
@@ -6,22 +6,31 @@ class Solution(object):
         :rtype: int
         """
         
-        # 2d DP
-        # shape [amount+1, len(coins)+1]
-        dp = [ [0]*(amount+1) for _ in range(len(coins)+1)]
-        
-        # 用0个硬币来拼凑0元, 这种情况是不存在的
+        dp =[[0]*(amount+1) for _ in range(len(coins)+1)]
+        # 初始化角落条件[0][0]
+        # 使用0个硬币构成0元的可能性为1.
         dp[0][0] = 1
         
-        for i in range(1,len(coins)+1):
-            for j in range(0, amount+1):
-                # laioffer 
-                # faster dynamic programming
+        # 初始化边界条件1
+        for i in range(1, len(coins)+1):
+            dp[i][0] = 1
+        
+        # 初始化边界条件2
+        for j in range(1, amount+1):
+            dp[0][j] = 0
+        
+        # 递推公式
+        for i in range(1, len(coins)+1):
+            for j in range(1, amount+1):
+                c = coins[i-1]
                 k = 0
-                while k*coins[i-1] <=j:
-                    
-                    dp[i][j] += dp[i-1][j-k*coins[i-1]]
+                while k*c<=j:
+                    dp[i][j] += dp[i-1][j-k*c]
                     k+=1
-                
+                # if j >=c:
+                #     dp[i][j] = dp[i-1][j]+dp[i][j-c]
+                # else:
+                #     dp[i][j] = dp[i-1][j]
         return dp[-1][-1]
-            
+        
+        
